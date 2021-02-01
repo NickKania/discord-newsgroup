12b19d9a6231d21d4e848d3eaea363ca23b062ca
Nick <nicholas.kania@maine.edu>
2021-02-01 15:27:37 -0500

    Working discord client connection

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+
+class CustomClient(discord.Client):
+    async def on_ready(self):
+        guild = discord.utils.get(client.guilds, name=GUILD)
+        print(f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})'
+            )
+
+client = CustomClient()
+client.run(TOKEN)
